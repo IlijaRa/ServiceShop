e4230f5016df5e60aa9e757b7479146d2ec12f10
IlijaRa <rakocevicilija7@gmail.com>
2022-06-18 00:33:39 +0200

    FinishedRequests-MainRepairer created - Prototype verson

---
+++ ServiceProcess/ServiceProcess/FinishedRequests-MainRepairer.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ServiceProcess
+{
+    /// <summary>
+    /// Interaction logic for FinishedRequests_MainRepairer.xaml
+    /// </summary>
+    public partial class FinishedRequests_MainRepairer : Window
+    {
+        public FinishedRequests_MainRepairer()
+        {
+            InitializeComponent();
+        }
+    }
+}
