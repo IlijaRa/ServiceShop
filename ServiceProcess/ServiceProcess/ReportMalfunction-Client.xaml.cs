ab75e296404bed399aca5027894ff3fb8b42f7d0
IlijaRa <rakocevicilija7@gmail.com>
2022-06-18 04:35:35 +0200

    ReportMalfunction-Client created - Prototype version

---
+++ ServiceProcess/ServiceProcess/ReportMalfunction-Client.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ServiceProcess
+{
+    /// <summary>
+    /// Interaction logic for ReportMalfunction_Client.xaml
+    /// </summary>
+    public partial class ReportMalfunction_Client : Window
+    {
+        public ReportMalfunction_Client()
+        {
+            InitializeComponent();
+        }
+    }
+}
